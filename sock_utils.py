8a02d2f6ba99e0c1e97f7571035be7eab09734aa
Ruben Branco <ruben.branco@outlook.pt>
2018-02-25 16:20:01 +0000

    Reformat

--- sock_utils.py
+++ sock_utils.py
@@ -2,6 +2,7 @@ import socket as s
 import traceback
 import sys
 
+
 def create_tcp_server_socket(address, port, queue_size):
     """Create a TCP socket for a server"""
     sock = s.socket(s.AF_INET, s.SOCK_STREAM)
@@ -16,6 +17,7 @@ def create_tcp_server_socket(address, port, queue_size):
 
     return sock
 
+
 def create_tcp_client_socket(address, port):
     """Create TCP Socket for a client"""
     sock = s.socket(s.AF_INET, s.SOCK_STREAM)
@@ -28,6 +30,7 @@ def create_tcp_client_socket(address, port):
         sys.exit(0)
 
     return sock
+
 
 def receive_all(socket, length):
     """Receives all packets, with a maximum length of length"""
