35d0fb45f242cc07f72236b1f361e96a68e32af3
GoncaloBISantos <32179963+GoncaloBISantos@users.noreply.github.com>
2018-02-26 13:13:29 +0000

    Add files via upload
    almost I think

--- lock_client.py
+++ lock_client.py
@@ -6,8 +6,39 @@ Grupo: ad007
 Números de aluno: 50006, 50013, 50019
 """
 # Zona para fazer imports
-
-
+import argparse
+import sock_utils
 
 # Programa principal
+class Client:
+	def __init__(self,host,port,client_id):
+		self.host = args.host
+		self.port = args.port
+		self.client_id = args.id
+	
+	def client_requests(self,host, port):
+                self.host = host
+                self.port = port
+		active = True
+		while active:
+			client_socket = sock_utils.creat_tcp_client_socket(host,port)
+			command = raw_input("comando > ")
+			if command == "exit":
+				active = false
+			else: 
+				client_socket.sendall(command)
+			resposta = sock.utils.receive_all(client_socket, 5000)
+                        print "Rsposta {}".format(resposta)
+
+
+
+
+if __name__ == "__main__":
+	description = "Gestor do Cliente"
+	parser = argparse.ArgumentParser(description = description)
+	parser.add_argument("host", type = str, metavar = "host", help = "O ip do servidor fornecedor de recursos")
+	parser.add_argument("port", type = int, metavar="port", help = "O porto que o cliente se quer ligar")
+	parser.add_argument("id do cliente", type = int, metavar = "id do cliente", help = "O id único do cliente")
+	args = parser.parse_args()
+
 
