c90f25420b98af23a07f2e38b277c49f05837311
Ruben Branco <ruben.branco@outlook.pt>
2018-05-08 21:39:58 +0100

    SSL Context for HTTPS/TLS

--- tv_series_manager/tv_series_manager/wsgi.py
+++ tv_series_manager/tv_series_manager/wsgi.py
@@ -1,4 +1,7 @@
 from .series_manager import application
+import ssl
 
 if __name__ == "__main__":
-    application.run()
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.load_cert_chain('../server.crt', '../server.key')
+    application.run(ssl_context=context)
