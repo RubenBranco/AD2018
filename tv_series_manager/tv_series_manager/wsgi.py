02ce7633cf905f37b62a03d1c116538178cd380e
Ruben Branco <ruben.branco@outlook.pt>
2018-05-10 14:25:23 +0100

    wsgi.py no longer needed due to environment demands

--- tv_series_manager/tv_series_manager/wsgi.py
+++
@@ -1,7 +0,0 @@
-from .series_manager import application
-import ssl
-
-if __name__ == "__main__":
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain('../server.crt', '../server.key')
-    application.run(ssl_context=context)
